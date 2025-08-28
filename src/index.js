bfdb608c92bfdc6d712fb3c7bb54166f950509d3
Gokul Vks <gokulvks@Gokuls-MacBook-Pro.local>
2025-08-28 10:01:50 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 async function cat(filePaths, outputFilePath) {
-  // write your code here
+  const fs = require('fs').promises;
+ for(let i of filePaths){
+    try{
+      if(i.includes('.txt')){
+        const data = await fs.readFile(i, 'utf8')
+        await fs.appendFile(outputFilePath, `${data}\n`)
+      }
+      else{
+        await fs.appendFile(outputFilePath,'Is a directory\n')
+      }
+  }
+    catch(err){
+      await fs.appendFile(outputFilePath,`File not found\n`)
+      console.error(err);
+    }
+  }
 }
 
 module.exports = { cat };
